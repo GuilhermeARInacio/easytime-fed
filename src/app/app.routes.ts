283c4c9cfd004acaa5c0067c7bf83bc61c47517e
vividsantos <viviane.familia834149@gmail.com>
2025-06-11 10:19:36 -0300

    feat: Tela de consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './componentes/login/login.component';
 import { TrocarSenhaComponent } from './componentes/trocar-senha/trocar-senha.component';
 import { EnviarCodigoComponent } from './componentes/enviar-codigo/enviar-codigo.component';
 import { BaterPontoComponent } from './componentes/bater-ponto/bater-ponto.component';
+import { ConsultaComponent } from './componentes/consulta/consulta.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
     { path: '', redirectTo:'/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
     { path: 'enviar-codigo', component: EnviarCodigoComponent },
     { path: 'trocar-senha', component: TrocarSenhaComponent },
-    { path: 'bater-ponto', component: BaterPontoComponent }
+    { path: 'bater-ponto', component: BaterPontoComponent },
+    { path: 'consulta', component: ConsultaComponent }
 
 ];
