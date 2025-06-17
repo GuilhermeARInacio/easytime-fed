4566564ea674aed025c99da09622292567e09c44
vividsantos <viviane.familia834149@gmail.com>
2025-06-17 08:55:47 -0300

    fix: Responsividade do modal de relatório concluida

--- src/app/componentes/modal-relatorio/modal-relatorio.component.spec.ts
+++ src/app/componentes/modal-relatorio/modal-relatorio.component.spec.ts
@@ -17,7 +17,7 @@ describe('ModalRelatorioComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o componente', () => {
     expect(component).toBeTruthy();
   });
 });
