1b8b5677c390cff45b68fbc0e4c92a643de3fd0f
vividsantos <viviane.familia834149@gmail.com>
2025-06-25 13:10:31 -0300

    feat: Modal de alteração

---
+++ src/app/componentes/modal-alteracao/modal-alteracao.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2  } from '@angular/core';
+import { RegistroPonto } from '../../interface/ponto/registro-ponto';
+import { CapitalizePipe } from "../../shared/capitalize.pipe";
+
+@Component({
+  selector: 'app-modal-alteracao',
+  imports: [CapitalizePipe],
+  templateUrl: './modal-alteracao.component.html',
+  styleUrl: './modal-alteracao.component.css'
+})
+export class ModalAlteracaoComponent {
+
+  @Output() fecharModal = new EventEmitter<void>();
+  @Input() usuario = '';
+  @Input() registro: RegistroPonto | undefined;
+  
+  constructor(
+    private renderer: Renderer2,
+    private element: ElementRef,
+  ) {}
+
+  @HostListener("document:keydown.escape") fecharModalComEsc(){
+    this.fecharModal.emit();
+    this.renderer.setStyle(this.element.nativeElement.ownerDocument.body, 'overflow', 'auto');
+  }
+
+  fechar() {
+    this.fecharModal.emit();
+    this.renderer.setStyle(this.element.nativeElement.ownerDocument.body, 'overflow', 'auto');
+  }
+
+}
