2e3671b7b449a4b5ed42224537df151655bbadce
vividsantos <viviane.familia834149@gmail.com>
2025-08-28 09:12:44 -0300

    feat: Adicionei um modal para desconectar o usuário caso ele se conecte em outra aba do navegador

--- src/app/componentes/menu-lateral/menu-lateral.component.ts
+++ src/app/componentes/menu-lateral/menu-lateral.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Host, HostListener, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Host, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
@@ -8,15 +8,37 @@ import { NavigationEnd, Router, RouterModule } from '@angular/router';
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.css'
 })
-export class MenuLateralComponent {
+export class MenuLateralComponent implements OnInit, OnDestroy {
   menuColapsado: boolean = true;
   role: string = localStorage.getItem('role') || '';
+  private storageListener: ((event: StorageEvent) => void) | undefined;
+  showSessionModal: boolean = false;
 
   constructor(
     private router: Router,
     private renderer: Renderer2,
     private element: ElementRef,
   ){}
+
+  ngOnInit() {
+    this.storageListener = (event: StorageEvent) => {
+      if (event.key === 'token') {
+        this.showSessionModal = true;
+      }
+    };
+    window.addEventListener('storage', this.storageListener);
+  }
+
+  ngOnDestroy() {
+    if (this.storageListener) {
+      window.removeEventListener('storage', this.storageListener);
+    }
+  }
+
+  confirmarLogout() {
+    this.showSessionModal = false;
+    this.sair();
+  }
 
   @HostListener("document:keydown.escape") fecharMenuComEsc() {
     if (!this.menuColapsado) {
